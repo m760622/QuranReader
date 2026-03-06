9e85d4e59e1f50a5ffa372c9abec06b747d8a2fe
m7606225 <m7606225@gmail.com>
2026-03-06 19:42:19 +0100

    Initial Commit

---
+++ QuranReader/QuranReaderApp.swift
@@ -0,0 +1,32 @@
+//
+//  QuranReaderApp.swift
+//  QuranReader
+//
+//  Created by Mohammed Abunada on 2026-03-06.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QuranReaderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ QuranReaderTests/QuranReaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuranReaderTests.swift
+//  QuranReaderTests
+//
+//  Created by Mohammed Abunada on 2026-03-06.
+//
+
+import Testing
+
+struct QuranReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
